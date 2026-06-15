a66b6cf8e213012dc3c593b1be34a69366f49f07
Dams93 <sochackidam93@gmail.com>
2026-06-16 01:59:03 +0200

    revert turbopack root path setup

--- apps/webshop/next.config.ts
+++ apps/webshop/next.config.ts
@@ -1,12 +1,8 @@
 import { composePlugins, withNx } from '@nx/next';
 import { WithNxOptions } from '@nx/next/plugins/with-nx';
-import path from 'path';
 
 const nextConfig: WithNxOptions = {
   nx: {},
-  turbopack: {
-    root: path.join(__dirname, '..', '..'),
-  },
   images: {
     domains: ['placehold.co'],
     dangerouslyAllowSVG: true,
